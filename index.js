46d1b123f631704726022989520d918f84a6f65c
maximconvey <maxim@convey.de>
2025-02-19 17:34:28 +0100

    Filter with params on LS_User und LS_Event

--- index.js
+++ index.js
@@ -5,3 +5,22 @@
 //  userName = conveyapisf1
 //  password = leadsuccess
 
+
+
+// filter LS_User
+
+//https://lstest.convey.de/apisftest/LS_Event?$format=json
+
+ // Filter parameters
+    // id = apitestappuser
+    // FirstName = appuser
+    // LastName = apitest
+    // EventId = 9b1763d8-1c4e-47ce-8903-41b74b0720e9
+
+// Url request 
+
+//https://lstest.convey.de/apisftest/LS_User?$format=json&$filter=Id eq 'apitestappuser' and FirstName eq 'appuser' and LastName eq 'apitest' and EventId eq '9b1763d8-1c4e-47ce-8903-41b74b0720e9'
+
+
+// filter LS_Event
+//https://lstest.convey.de/apisftest/LS_Event?$format=json&$filter=Id eq '9b1763d8-1c4e-47ce-8903-41b74b0720e9' and FirstName eq 'appuser' and LastName eq 'apitest' and EventId eq '9b1763d8-1c4e-47ce-8903-41b74b0720e9'
