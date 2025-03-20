f2baa2cb0aae7dac7d25dd607d54b2a766e3cb64
maximconvey <maxim@convey.de>
2025-03-20 09:22:51 +0100

    add cors url

--- salesforce-backend/server.js
+++ salesforce-backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 app.use(cors({
-  origin: true, 
+  origin: ['https://delightful-desert-016e2a610.4.azurestaticapps.net', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Cache-Control', 'X-Session-Token']
